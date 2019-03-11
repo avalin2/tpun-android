a35bc0b4ca2efc5b517e0c1440eed68eb74ae8f8
Alexandre Valin <alexandre.valin@live.fr>
2019-03-11 14:47:02 +0100

    New version

--- app/src/main/java/alexandre/valin/tpone/MainActivity.java
+++ app/src/main/java/alexandre/valin/tpone/MainActivity.java
@@ -1,5 +1,6 @@
 package alexandre.valin.tpone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button validForm ;
+    private EditText nom;
+    private EditText prenom;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +26,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        this.nom =findViewById(R.id.nom_form);
+        this.prenom=findViewById(R.id.prenom_form);
+        this.validForm = findViewById(R.id.button_valAcc);
+
+        validForm.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent secondActi = new Intent(getApplicationContext(),ViewContact.class);
+                startActivity(secondActi);
+                finish();
             }
         });
 
-        // j'ajoute un commentaire
+
+
     }
 
     @Override
